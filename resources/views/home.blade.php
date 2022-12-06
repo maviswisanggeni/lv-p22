8b9d7f0ad15c1431c106c0a6fffae61f72f3fd66
WilliAndHan <william.a.hanantha@gmail.com>
2022-12-06 10:10:47 +0700

    Laravel 2022

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
     <h1>Home</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,quae.</p>
 @endsection
