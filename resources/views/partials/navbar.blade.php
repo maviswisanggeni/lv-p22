a8515122ee25bbbfa0c52b5031b790d4152525f1
Mavis Daffa Wisanggeni <mavisdaffawisanggeni22@gmail.com>
2022-12-07 10:47:57 +0700

    modify: [MW] modify home page

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -73,9 +73,9 @@
                             id="user-menu-button" aria-expanded="false" aria-haspopup="true"
                             onclick="toggleNavbar('profile-menu')">
                             <span class="sr-only">Open user menu</span>
+                            {{-- create image https://f.top4top.io/p_2532nxbro1.jpg cover full fill 50 x 50 rounded --}}
                             <img class="h-8 w-8 rounded-full"
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                alt="">
+                                src="https://j.top4top.io/p_2532dx6yy1.jpg" alt="">
                         </button>
                     </div>
 
